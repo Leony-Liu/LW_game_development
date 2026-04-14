8f9d796f5588dc793dae2cdb0660dc02ec61ce7a
Maluxiye <1289040549@qq.com>
2026-04-14 15:17:18 +0800

    给地图敌人添加所在TileMapLayer信息

--- Scene/DungeonScene/Script/enemie_set.gd
+++ Scene/DungeonScene/Script/enemie_set.gd
@@ -27,9 +27,11 @@ func spawn_enemies_in_rooms(leaf_node: Array[BSPNode], world_obstacle: Dictionar
 					var enemy = enemy_scene.instantiate()
 					enemy.get_node("EnemyManager").MyNode = node
 					enemy.get_node("Information").ID = mySeed.randi_range(20001, 2100)
+					enemy.get_node("Information").TileMapReference = tilemap
 					add_child(enemy)
 					enemy.add_to_group("Enemies")
 					enemy.global_position = get_enemy_set_pos(node, world_obstacle)
+					
 	pass
 
 
--- Scene/Enemy_Dungeon_Scene/Script/enemy_dungeo_information.gd
+++ Scene/Enemy_Dungeon_Scene/Script/enemy_dungeo_information.gd
@@ -2,3 +2,4 @@ extends Node
 
 @export var ID: int
 @export var BattleID: int = 20001
+@export var TileMapReference: TileMapLayer
